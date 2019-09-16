5f047428fc109e7d16be7afdb5bab82e848125c8
Nothingvrs <b00M_vrs@mail.ru>
2019-09-17 00:39:56 +0300

    2 задание

---
+++ js/stat.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var GAP_X = 30;
+var GAP_Y = 40;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_HEIGHT = 20;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+
+
+window.renderStatistics = function(ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = "normal 16pt PT Mono";
+  ctx.fillText("Ура вы победили!",CLOUD_X + GAP_X,CLOUD_Y + GAP_X );
+  ctx.fillText("Список результатов:",CLOUD_X + GAP_X,CLOUD_Y + GAP_Y + GAP);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillText(players[i], CLOUD_X + GAP_Y + (GAP_Y + TEXT_WIDTH) * i , CLOUD_Y + CLOUD_HEIGHT - GAP_X);
+    ctx.fillRect (CLOUD_X + GAP_Y + (GAP_Y + TEXT_WIDTH) * i,150 + GAP_Y + GAP_X - (150 * times[i] / maxTime),40,150 * times[i] / maxTime);
+    players[i] == 'Вы' ? ctx.fillStyle = ('rgba(255, 0, 0, 1)') :  ctx.fillStyle = ('rgba(0, 0, 255, ' + Math.random( ) + ')');
+   }
+
+
+};
