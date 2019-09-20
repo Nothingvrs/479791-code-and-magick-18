61cd6cc83e69ced1876fa41167d00f236e810573
Nothingvrs <b00M_vrs@mail.ru>
2019-09-20 22:43:54 +0300

    Вроде всё))

--- js/stat.js
+++ js/stat.js
@@ -8,6 +8,8 @@ var GAP = 10;
 var GAP_X = 30;
 var GAP_Y = 40;
 var TEXT_WIDTH = 50;
+var barWidth = 40;
+var maxHeightGistogram = 150;
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -22,9 +24,12 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+function getRandomInt(min, max) {
+  return Math.random() * (max - min + 1) + min;
+}
+
 var getColorGistogramm = function (name) {
-  var color = (name === 'Вы') ? '#f00' : 'rgba(0, 0, 255, ' + Math.random() + ')';
-  return color;
+  return (name === 'Вы') ? '#f00' : 'rgba(0, 0, 255, ' + getRandomInt(0.3, 1) + ')';
 };
 
 var drawText = function (ctx, text, textX, textY) {
@@ -32,12 +37,13 @@ var drawText = function (ctx, text, textX, textY) {
 };
 
 var getDrawStatistics = function (ctx, name, time, maximumTime, index) {
-  var barHeight = 150 * time / maximumTime;
+  var barHeight = maxHeightGistogram * time / maximumTime;
+  var maximumGap = CLOUD_X + GAP_Y + (GAP_Y + TEXT_WIDTH) * index;
   ctx.fillStyle = '#000';
-  drawText(ctx, name, CLOUD_X + GAP_Y + (GAP_Y + TEXT_WIDTH) * index, CLOUD_Y + CLOUD_HEIGHT - GAP_X);
-  drawText(ctx, Math.floor(time), CLOUD_X + GAP_Y + (GAP_Y + TEXT_WIDTH) * index, CLOUD_Y + 150 + GAP_Y + GAP_X - barHeight);
+  drawText(ctx, name, maximumGap, CLOUD_Y + CLOUD_HEIGHT - GAP);
+  drawText(ctx, Math.floor(time), maximumGap, CLOUD_Y + maxHeightGistogram + GAP_Y + GAP_X - barHeight);
   ctx.fillStyle = getColorGistogramm(name);
-  ctx.fillRect(CLOUD_X + GAP_Y + (GAP_Y + TEXT_WIDTH) * index, 150 + GAP_Y + GAP_Y - barHeight, 40, barHeight);
+  ctx.fillRect(maximumGap, CLOUD_Y + CLOUD_HEIGHT - GAP_Y - barHeight, barWidth, barHeight);
 };
 
 window.renderStatistics = function (ctx, players, times) {
